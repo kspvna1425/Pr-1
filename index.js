58a6fc19a1d86df2469c5685717a3acb57162bb4
Vikash Raghav <104614541+kspvna1425@users.noreply.github.com>
2024-02-10 23:35:07 +0530

    Navigation-bar main file
    demo pr-1

---
+++ index.js
@@ -0,0 +1,12 @@
+function myfunction() {
+    var x = document.getElementById("menu")
+    if(x.style.display == "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
+
+let elem = document.getElementsByClassName('span');
+console.log(elem)
